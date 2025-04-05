adb2f481a9ff47dbe3252b2edd39f829b5b34a9e
Aimee O'Sullivan-Smith <aimee.osullivan-smith@student.manchester.ac.uk>
2025-04-05 19:34:19 +0100

    styling for report page complete

--- src/Report.jsx
+++ src/Report.jsx
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CssFiles/Report.css";
 import { Helmet } from "react-helmet";
 
 export default function Report() {
-  <Helmet>
-  <title>Report</title>
-</Helmet>
-  return (
+  const [name, setReportName] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
 
-    <div className="Report">
-        Report
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setReportName("");
+    setDate("");
+    setDescription("");
+
+    alert("Thank you for your loyalty, citizen.");
+  };
+  
+  return (
+    <div className="login-container">
+      <Helmet>
+        <title>Report</title>
+      </Helmet>
+      
+      <div className="report-box">
+        <h1 className="report-title">Citizen Report</h1>
+        <div className='report-information-container'>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label>REPORT NAME:</label>
+              <input 
+                type="text" 
+                className="brutal-input"
+                style={{ width: "500px", height: "40px", padding: "5px", boxSizing: "border-box"}}
+                value={name}
+                onChange={(e) => setReportName(e.target.value)}
+                required
+              />
+              <label>DATE:</label>
+              <input 
+                type="text" 
+                className="brutal-input"
+                style={{ width: "500px", height: "40px", padding: "5px", boxSizing: "border-box"}}
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                required
+              />
+              <label>DESCRIPTION:</label>
+              <textarea 
+                type="text" 
+                className="brutal-input"
+                style={{ width: "500px", height: "120px", padding: "5px", boxSizing: "border-box"}}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="brutal-button">
+            SUBMIT
+          </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
