d66c611dbac4a7901677c737a51a2ef6e4abbe21
Aimee O'Sullivan-Smith <aimee.osullivan-smith@student.manchester.ac.uk>
2025-04-05 15:22:54 +0100

    rebasing with main initial changes

--- src/Report.jsx
+++ src/Report.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import "./CssFiles/Report.css";
+import { Helmet } from "react-helmet";
 
 export default function Report() {
-
+  <Helmet>
+  <title>Report</title>
+</Helmet>
   return (
+
     <div className="Report">
         Report
     </div>
-  );
-}import React from "react"
-import { Helmet } from "react-helmet";
-
-export default function Login() {
-
-  return (
-    <>
-    <Helmet>
-        <title>Report</title>
-    </Helmet>
-    hello Report
-    </>
   );
 }
