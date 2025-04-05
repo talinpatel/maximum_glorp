39fdc3e14579cb54336fe59c7380407c67a71ead
p30129tp <talin.patel@student.manchester.ac.uk>
2025-04-05 16:57:28 +0100

    fleshed out profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,14 +1,26 @@
 import React from "react"
 import { Helmet } from "react-helmet";
+import "./CssFiles/Profile.css"
 
 export default function Profile() {
 
   return (
     <>
     <Helmet>
-        <title>Profile</title>
+        <title>Your profile</title>
     </Helmet>
-    hello Profile
+    <div className="profile-container">
+        <div className="profile-box">
+            <h1 className="profile-title">
+                Hello __User__
+            </h1>
+            <p className="profile-text">name</p>
+            <p className="profile-text">location</p>
+            <p className="profile-text">social credits</p>
+            <p className="profile-text">info</p>
+            <p className="profile-text">notable contributions</p>
+        </div>
+    </div>
     </>
   );
 }
