6bb68570ccfb5487e18a06c92cd01d9cd1d2f147
Aimee O'Sullivan-Smith <aimee.osullivan-smith@student.manchester.ac.uk>
2025-04-05 15:11:16 +0100

    intial reporting page setup

--- src/Report.jsx
+++ src/Report.jsx
@@ -8,4 +8,17 @@ export default function Report() {
         Report
     </div>
   );
+}import React from "react"
+import { Helmet } from "react-helmet";
+
+export default function Login() {
+
+  return (
+    <>
+    <Helmet>
+        <title>Report</title>
+    </Helmet>
+    hello Report
+    </>
+  );
 }
