bcb5d663d3b9aef78c94c8e5bf1e40f07d1807f8
p30129tp <talin.patel@student.manchester.ac.uk>
2025-04-05 17:32:32 +0100

    added image to profile page

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import { Helmet } from "react-helmet";
 import "./CssFiles/Profile.css"
+import profile_photo from "./images/profile_photo.png"
+
+// const profile_photo = require('./images/profile_photo.png');
 
 export default function Profile() {
 
@@ -11,14 +14,21 @@ export default function Profile() {
     </Helmet>
     <div className="profile-container">
         <div className="profile-box">
-            <h1 className="profile-title">
-                Hello __User__
-            </h1>
-            <p className="profile-text">name</p>
-            <p className="profile-text">location</p>
-            <p className="profile-text">social credits</p>
-            <p className="profile-text">info</p>
-            <p className="profile-text">notable contributions</p>
+            <div className="profile-text-container">
+                <h1 className="profile-title">
+                    Hello __User__
+                </h1>
+                <p className="profile-text">name</p>
+                <p className="profile-text">location</p>
+                <p className="profile-text">social credits</p>
+                <p className="profile-text">info</p>
+                <p className="profile-text">notable contributions</p>
+            </div>
+
+            <div className="profile-img-container">
+                <img src={profile_photo}/>
+            </div>
+
         </div>
     </div>
     </>
