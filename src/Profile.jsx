43ef83ae200391d91cd815ae86950be3d9188c97
p30129tp <talin.patel@student.manchester.ac.uk>
2025-04-05 15:00:47 +0100

    added and connected profile page

---
+++ src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Helmet } from "react-helmet";
+
+export default function Profile() {
+
+  return (
+    <>
+    <Helmet>
+        <title>Profile</title>
+    </Helmet>
+    hello Profile
+    </>
+  );
+}
